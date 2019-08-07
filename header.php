7ac7aba162f864631bb696dfa9643b723b5de6ec
Matt Jones <matt.jones@tvgla.com>
2019-08-07 13:12:23 -0700

    removed tweenmax font line-height

--- header.php
+++ header.php
@@ -5,7 +5,6 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/main.min.css" />
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.20.1/TweenMax.min.js"></script>
 
 		<script src="<?php echo get_template_directory_uri(); ?>/js/main.min.js"></script>
 		<?php 
